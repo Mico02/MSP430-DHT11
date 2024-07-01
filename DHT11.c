fa3f4d97bf77125ff7302af5e234813fc0ccb6a1
Mico02 <mohamed.hussein122702@gmail.com>
2024-07-01 16:39:06 -0400

    added comments

--- DHT11.c
+++ DHT11.c
@@ -8,7 +8,14 @@
 #include "driverlib.h"
 
 /****** HELPER FUNCTIONS *****/
+
+/**
+ * Sets a GPIO pin as output given its port and pin.
+ * @param port : port of pin
+ * @param pin  : pin number
+ */
 void setPortOutput(int port, int pin){
+    //Checks what port was chosen and SETS the bit for the corresponding pin in PxDIR register
     switch(port){
         case 1:
             P1DIR |= (1 << pin);
@@ -42,7 +49,14 @@ void setPortOutput(int port, int pin){
     }
 }
 
+
+/**
+ * Sets a GPIO pin as input given its port and pin.
+ * @param port : port of pin
+ * @param pin  : pin number
+ */
 void setPortInput(int port, int pin){
+    //Checks what port was chosen and RESETS the bit for the corresponding pin in PxDIR register
     switch(port){
         case 1:
             P1DIR &= ~(1 << pin);
@@ -76,82 +90,100 @@ void setPortInput(int port, int pin){
     }
 }
 
+
+/**
+ * Sets a GPIO pin as HIGH given its port and pin, pin must be set at output
+ * @param port : port of pin
+ * @param pin  : pin number
+ */
 void setPinHigh(int port, int pin){
+    //Checks what port was chosen and SETS the bit for the corresponding pin in PxOUT register
     switch(port){
-            case 1:
-                P1OUT |= (1 << pin);
-                break;
-            case 2:
-                P2OUT |= (1 << pin);
-                break;
-            case 3:
-                P3OUT |= (1 << pin);
-                break;
-            case 4:
-                P4OUT |= (1 << pin);
-                break;
-            case 5:
-                P5OUT |= (1 << pin);
-                break;
-            case 6:
-                P6OUT |= (1 << pin);
-                break;
-            case 7:
-                P7OUT |= (1 << pin);
-                break;
-            case 8:
-                P8OUT |= (1 << pin);
-                break;
-            case 9:
-                P9OUT |= (1 << pin);
-                break;
-            default:
-                break;
-        }
+		case 1:
+		    P1OUT |= (1 << pin);
+		    break;
+		case 2:
+		    P2OUT |= (1 << pin);
+		    break;
+		case 3:
+		    P3OUT |= (1 << pin);
+		    break;
+		case 4:
+		    P4OUT |= (1 << pin);
+		    break;
+		case 5:
+		    P5OUT |= (1 << pin);
+		    break;
+		case 6:
+		    P6OUT |= (1 << pin);
+		    break;
+		case 7:
+		    P7OUT |= (1 << pin);
+		    break;
+		case 8:
+		    P8OUT |= (1 << pin);
+		    break;
+		case 9:
+		    P9OUT |= (1 << pin);
+		    break;
+		default:
+		    break;
+    }
 }
 
 
+/**
+ * Sets a GPIO pin as LOW given its port and pin, pin must be set at output
+ * @param port : port of pin
+ * @param pin  : pin number
+ */
 void setPinLow(int port, int pin){
+    //Checks what port was chosen and RESETS the bit for the corresponding pin in PxOUT register
     switch(port){
-            case 1:
-                P1OUT &= ~(1 << pin);
-                break;
-            case 2:
-                P2OUT &= ~(1 << pin);
-                break;
-            case 3:
-                P3OUT &= ~(1 << pin);
-                break;
-            case 4:
-                P4OUT &= ~(1 << pin);
-                break;
-            case 5:
-                P5OUT &= ~(1 << pin);
-                break;
-            case 6:
-                P6OUT &= ~(1 << pin);
-                break;
-            case 7:
-                P7OUT &= ~(1 << pin);
-                break;
-            case 8:
-                P8OUT &= ~(1 << pin);
-                break;
-            case 9:
-                P9OUT &= ~(1 << pin);
-                break;
-            default:
-                break;
+        case 1:
+            P1OUT &= ~(1 << pin);
+            break;
+        case 2:
+            P2OUT &= ~(1 << pin);
+            break;
+        case 3:
+            P3OUT &= ~(1 << pin);
+            break;
+        case 4:
+            P4OUT &= ~(1 << pin);
+            break;
+        case 5:
+            P5OUT &= ~(1 << pin);
+            break;
+        case 6:
+            P6OUT &= ~(1 << pin);
+            break;
+        case 7:
+            P7OUT &= ~(1 << pin);
+            break;
+        case 8:
+            P8OUT &= ~(1 << pin);
+            break;
+        case 9:
+            P9OUT &= ~(1 << pin);
+            break;
+        default:
+            break;
         }
 }
 
 
-
+/**
+ * Initializes the DHT sensor on the given port and pin
+ * @param port : port of pin
+ * @param pin  : pin number
+ */
 void DHT11_init(DHT11* sensor, int port, int pin){
+    //setting struct variables to match port & pin
     sensor->port = port;
     sensor->pin = pin;
-    WDT_A_hold(WDT_A_BASE);
-    PMM_unlockLPM5();
+    WDT_A_hold(WDT_A_BASE); // disable wdt
+    PMM_unlockLPM5(); // unlock all pins
 }
 
 
