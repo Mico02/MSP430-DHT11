fabe9ce8fcccfb86487e7172842d838e7167c3db
Mico02 <mohamed.hussein122702@gmail.com>
2024-07-01 16:07:59 -0400

    defined dht functions

--- DHT11.h
+++ DHT11.h
@@ -5,11 +5,20 @@
  *      Author: mohamedhussein
  */
 
+
 #ifndef DHT11_H_
 #define DHT11_H_
 
 
+typedef struct{
+    int port;
+    int pin;
+} DHT11;
 
+void DHT11_init(DHT11* sensor, int port, int pin);
+float DHT11_readTemperature(DHT11* sensor);
+float DHT11_readHumidity(DHT11* sensor);
+void DHT11_readTempHumd(DHT11* sensor, float* temperature, float* humidity);
 
 
 #endif /* DHT11_H_ */
