134292ad6eaaa7e0e3f5467a71e6aea67ca5fb17
Mico02 <mohamed.hussein122702@gmail.com>
2024-07-01 16:31:25 -0400

    completed helper functions

--- DHT11.c
+++ DHT11.c
@@ -76,12 +76,82 @@ void setPortInput(int port, int pin){
     }
 }
 
+void setPinHigh(int port, int pin){
+    switch(port){
+            case 1:
+                P1OUT |= (1 << pin);
+                break;
+            case 2:
+                P2OUT |= (1 << pin);
+                break;
+            case 3:
+                P3OUT |= (1 << pin);
+                break;
+            case 4:
+                P4OUT |= (1 << pin);
+                break;
+            case 5:
+                P5OUT |= (1 << pin);
+                break;
+            case 6:
+                P6OUT |= (1 << pin);
+                break;
+            case 7:
+                P7OUT |= (1 << pin);
+                break;
+            case 8:
+                P8OUT |= (1 << pin);
+                break;
+            case 9:
+                P9OUT |= (1 << pin);
+                break;
+            default:
+                break;
+        }
+}
+
+
+void setPinLow(int port, int pin){
+    switch(port){
+            case 1:
+                P1OUT &= ~(1 << pin);
+                break;
+            case 2:
+                P2OUT &= ~(1 << pin);
+                break;
+            case 3:
+                P3OUT &= ~(1 << pin);
+                break;
+            case 4:
+                P4OUT &= ~(1 << pin);
+                break;
+            case 5:
+                P5OUT &= ~(1 << pin);
+                break;
+            case 6:
+                P6OUT &= ~(1 << pin);
+                break;
+            case 7:
+                P7OUT &= ~(1 << pin);
+                break;
+            case 8:
+                P8OUT &= ~(1 << pin);
+                break;
+            case 9:
+                P9OUT &= ~(1 << pin);
+                break;
+            default:
+                break;
+        }
+}
 
 
 
 void DHT11_init(DHT11* sensor, int port, int pin){
     sensor->port = port;
     sensor->pin = pin;
+    WDT_A_hold(WDT_A_BASE);
+    PMM_unlockLPM5();
 }
 
 
